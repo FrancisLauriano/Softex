091da6b1a7cd033746154e18b3cfaa027c800c6b
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2023-08-02 15:48:58 -0300

    Delete Modulo 2 directory

--- Modulo 2/1-cont_bomba.js
+++
@@ -1,9 +0,0 @@
-
-console.log("Iniciando contagem regressiva...")
-for (let contagem=10; contagem>=1; contagem--) {
-        console.log(contagem,"","seg");
-}
-if(contagem=1) {
-        console.log("BUM!!!")
-}
-
