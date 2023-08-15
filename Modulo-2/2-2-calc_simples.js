e55111f1b538cd1e53bb034b2f12f905006f2c3e
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2023-08-14 23:37:56 -0300

    Update 2-2-calc_simples.js

--- Modulo-2/2-2-calc_simples.js
+++ Modulo-2/2-2-calc_simples.js
@@ -1,37 +1,63 @@
 const readline = require('readline-sync');
 const prompt = require('prompt-sync')();
 
-function calculadora(){
-    function entradaDados(){
-        console.log("=== Calculadora ===");
-        console.log("Informe 1º número: "); 
-        let num1 = readline.questionFloat(); 
-        console.log("Informe 2º número: "); 
-        let num2 = readline.questionFloat(); 
-        console.log("Digite o número referente a operação que deseja realizar:"); 
-        console.log(" 1.Soma\n 2.Subtração\n 3.Multiplicação\n 4.Divisão"); 
-        let operacao = readline.questionInt();
+function calculadora (){
 
-        return {num1, num2, operacao};
-    }
-
-    function tipOperacao(num1, num2, operacao){
-        switch (operacao) {
-            case 1:
-                return num1+num2;
-            case 2:
-                return num1-num2;
-            case 3:
-                return num1*num3;
-            case 4:
-                return num1/num2;
-            default:
-                return 0;
+    function soma(num1, num2){
+        return num1+num2
         }
-    } 
-let {num1, num2, operacao} = entradaDados ();
-let resultado = tipOperacao (num1, num2, operacao);   
-console.log("==> Resultado: ", resultado);
+    function sub(num1, num2){
+        return num1-num2
+        }
+    function mult(num1, num2){
+        return num1*num2
+        }
+    function div(num1, num2){
+        return num1/num2
+        }
+
+
+    while(true) {
+        console.log("========== Calculadora ==========");
+        console.log("Digite o número referente a ação que deseja realizar:"); 
+        let operacao = console.log("\t1.Soma\n \t2.Subtração\n \t3.Multiplicação\n \t4.Divisão\n \t0.Sair"); 
+        operacao = readline.questionInt()
+            if(operacao===0) {
+                console.log("Finalizado...Até mais!");
+                break;
+            } else if (operacao===1) {
+                console.log("Informe 1º número: "); 
+                let num1 = readline.questionFloat(); 
+                console.log("Informe 2º número: "); 
+                let num2 = readline.questionFloat();
+                console.log("Resultado:", soma(num1,num2));
+                break;
+            } else if(operacao===2) {
+                console.log("Informe 1º número: "); 
+                let num1 = readline.questionFloat(); 
+                console.log("Informe 2º número: "); 
+                let num2 = readline.questionFloat();
+                console.log("Resultado:", sub(num1,num2));
+                break;
+            } else if(operacao===3) {
+                console.log("Informe 1º número: "); 
+                let num1 = readline.questionFloat(); 
+                console.log("Informe 2º número: "); 
+                let num2 = readline.questionFloat();
+                console.log("Resultado:", mult(num1,num2));
+                break;
+            } else if(operacao===4) {
+                console.log("Informe 1º número: "); 
+                let num1 = readline.questionFloat(); 
+                console.log("Informe 2º número: "); 
+                let num2 = readline.questionFloat();
+                console.log("Resultado:", div(num1,num2));
+                break;
+            }else{
+                operacao = console.log("==> ESSA OPÇÃO NÃO EXISTE <==\n Informe uma opção válida abaixo!");
+                continue;
+            }
+        }
 }
 
 calculadora();
