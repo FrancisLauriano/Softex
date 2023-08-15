a8203bce633161e412bdbb4e66342e5696fec6da
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2023-08-15 14:34:08 +0000

    atualizado em 15-08-2023

---
+++ 13-erros.js
@@ -0,0 +1,67 @@
+// // throw 'minha Exececao';
+// // throw true;
+// // throw 42;
+
+// try {
+//     criticalCode()
+    
+//   } catch (error) {
+//     console.log(`[Erro]`);
+//     console.log(error.message);
+    
+//   } finally {
+  
+//   }
+  
+  
+//   try {
+//     criticalCode()
+    
+//   } catch (e) {
+//     console.log(`[Erro]`);
+//     console.log(e.message);
+    
+//   } finally {
+//     console.log(`Aqui sempre será executado ...`);
+  
+//   }
+
+
+// try {
+//     criticalCode()
+    
+//   } catch (error) {
+//     console.log(`[Erro]`);
+//     console.log(error.message);
+    
+//   } finally {
+  
+//   }
+  
+  
+//   try {
+//     criticalCode()
+    
+//   } catch (e) {
+//     console.log(`[Erro]`);
+//     console.log(e.message);
+    
+//   } finally {
+//     console.log(`Aqui sempre será executado ...`);
+  
+//   }
+
+
+  try {
+    const divisor = 0;
+    const resultado = 10; 
+    console.log(`O resultado é; ${resultado}`);
+    
+  } catch (error) {
+    console.error("Erro:", error.message);
+  }
+    
+//   } finally {
+//     console.log(`Aqui sempre será executado ...`);
+  
+//   }
