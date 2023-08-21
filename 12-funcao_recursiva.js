3c6d03ed70d27ddea5cb1be0b88302d396f2b029
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2023-08-21 14:06:22 -0300

    atualizado 15-08-2023

---
+++ 12-funcao_recursiva.js
@@ -0,0 +1,36 @@
+// function somaRecursiva (n) {
+//    if(n <= 0) {
+//     return 0;
+//    }else{
+//     return n + somaRecursiva(n-1);
+//     }
+// }
+
+// let numero = 10;
+// console.log(somaRecursiva(numero));
+
+// function fibonacci(n){
+//     if (n === 1){
+//         return 1;
+//     }else if (n <= 0){
+//         return 0;
+//     }else{
+//         return fibonacci(n-1) + fibonacci(n-2);
+//     }
+// }
+// let numero2 = 8;
+// console.log(`O ${numero2}-ésimo termo da sequência de Fibonacci é ${fibonacci(numero2)}`);
+
+
+  // try {
+  //   const divisor = 0;
+  //   const resultado = 10 / divisor;
+  //   console.log(`O resultado é ${resultado}`);
+    
+  // } catch (error) {
+  //   console.error("Erro:", error.message);
+  // } finally {
+  //   console.log(`O divisor não pode ser zero!`);
+  // }
+    
+
