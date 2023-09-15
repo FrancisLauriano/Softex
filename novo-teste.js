02ff9d2f941d5194474743b4ec3b5e96285b4788
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2023-09-15 16:19:54 -0300

    15-09-2023

--- novo-teste.js
+++ novo-teste.js
@@ -1,225 +1,268 @@
 const readline = require('readline-sync');
+
 const Adm = [];
 const Aluno = [];
 const Emprestimo = [];
 const Instrumento = [];
 const Manutencao = [];
 
-
-// CRUD ADM
 // Função para validar entrada de dados
 function validarEntradaDeDados(dados, camposObrigatorios) {
-	for (const campo of camposObrigatorios) {
-			if (!dados[campo]) {
-					console.log(`O campo '${campo}' é obrigatório.`);
-					return false;
-			}
-	}
-	return true;
+  for (const campo of camposObrigatorios) {
+    if (!dados[campo]) {
+      console.log(`O campo '${campo}' é obrigatório.`);
+      return false;
+    }
+  }
+  return true;
 }
 
 // Função para criar um ADM com validação de entrada de dados
 function criarAdm() {
-	console.log('\n** Criar ADM **');
-	const novoAdm = {
-			nome: readline.question('Digite o nome do ADM: '),
-			email: readline.question('Digite o email do ADM: '),
-			senha: readline.question('Digite a senha do ADM: '),
-	};
+  console.log('\n** Criar ADM **');
+  const novoAdm = {
+    nome: readline.question('Digite o nome do ADM: '),
+    email: readline.question('Digite o email do ADM: '),
+    senha: readline.question('Digite a senha do ADM: '),
+  };
 
-	if (validarEntradaDeDados(novoAdm, ['nome', 'email', 'senha'])) {
-			try {
-					Adm.push(novoAdm);
-					console.log('ADM criado com sucesso:', novoAdm);
-			} catch (error) {
-					console.error('Erro ao criar ADM:', error);
-			}
-	}
+  if (validarEntradaDeDados(novoAdm, ['nome', 'email', 'senha'])) {
+    try {
+      Adm.push(novoAdm);
+      console.log('ADM criado com sucesso:', novoAdm);
+    } catch (error) {
+      console.error('Erro ao criar ADM:', error);
+    }
+  }
 }
 
 // Função para listar ADMs
 function listarAdms() {
-	console.log('\n** Listar ADMs **');
-	try {
-			if (Adm.length > 0) {
-					console.log('Lista de ADMs:');
-					Adm.forEach((adm, index) => {
-							console.log(`ID: ${index}, Nome: ${adm.nome}, Email: ${adm.email}`);
-					});
-			} else {
-					console.log('Nenhum ADM encontrado.');
-			}
-	} catch (error) {
-			console.error('Erro ao listar ADMs:', error);
-	}
+  console.log('\n** Listar ADMs **');
+  try {
+    if (Adm.length > 0) {
+      console.log('Lista de ADMs:');
+      Adm.forEach((adm, index) => {
+        console.log(`ID: ${index}, Nome: ${adm.nome}, Email: ${adm.email}`);
+      });
+    } else {
+      console.log('Nenhum ADM encontrado.');
+    }
+  } catch (error) {
+    console.error('Erro ao listar ADMs:', error);
+  }
 }
 
 // Função para buscar ADM por Nome
 function buscarAdmPorNome() {
-	console.log('\n** Buscar ADM por Nome **');
-	const nome = readline.question('Digite o nome do ADM que deseja buscar: ');
+  console.log('\n** Buscar ADM por Nome **');
+  const nome = readline.question('Digite o nome do ADM que deseja buscar: ');
 
-	try {
-			const admsEncontrados = Adm.filter((adm) => adm.nome === nome);
+  try {
+    const admsEncontrados = Adm.filter((adm) => adm.nome === nome);
 
-			if (admsEncontrados.length > 0) {
-					console.log('ADM(s) encontrado(s):');
-					admsEncontrados.forEach((adm, index) => {
-							console.log(`ID: ${index}, Nome: ${adm.nome}, Email: ${adm.email}`);
-					});
-			} else {
-					console.log('Nenhum ADM encontrado com o nome fornecido.');
-			}
-	} catch (error) {
-			console.error('Erro ao buscar ADM por nome:', error);
-	}
+    if (admsEncontrados.length > 0) {
+      console.log('ADM(s) encontrado(s):');
+      admsEncontrados.forEach((adm, index) => {
+        console.log(`ID: ${index}, Nome: ${adm.nome}, Email: ${adm.email}`);
+      });
+    } else {
+      console.log('Nenhum ADM encontrado com o nome fornecido.');
+    }
+  } catch (error) {
+    console.error('Erro ao buscar ADM por nome:', error);
+  }
 }
 
 // Função para atualizar ADM
 function atualizarAdm() {
-	console.log('\n** Atualizar ADM **');
-	const id = readline.question('Digite o ID do ADM a ser atualizado: ');
+  console.log('\n** Atualizar ADM **');
+  const nomeOuEmail = readline.question('Digite o nome ou email do ADM que deseja atualizar: ');
 
-	if (!Adm[id]) {
-			console.log('ADM não encontrado para atualização.');
-			return;
-	}
+  try {
+    // Procurar o ADM com base no nome ou email fornecido
+    const admParaAtualizar = Adm.find((adm) => adm.nome === nomeOuEmail || adm.email === nomeOuEmail);
 
-	const admParaAtualizar = Adm[id];
-	const novoNome = readline.question('Digite o novo nome (ou pressione Enter para manter o mesmo): ');
-	const novoEmail = readline.question('Digite o novo email (ou pressione Enter para manter o mesmo): ');
-	const novaSenha = readline.question('Digite a nova senha: ');
+    if (!admParaAtualizar) {
+      console.log('ADM não encontrado para atualização.');
+      return;
+    }
 
-	const novosDados = {};
+    // Mostrar os dados atuais do ADM
+    console.log('Dados atuais do ADM:');
+    console.log(`Nome: ${admParaAtualizar.nome}`);
+    console.log(`Email: ${admParaAtualizar.email}`);
+    console.log(`Senha: ${admParaAtualizar.senha}`);
 
-	if (novoNome) {
-			novosDados.nome = novoNome;
-	}
-	if (novoEmail) {
-			novosDados.email = novoEmail;
-	}
-	if (novaSenha) {
-			novosDados.senha = novaSenha;
-	}
+    console.log('\nO que deseja atualizar?');
+    console.log('1. Nome');
+    console.log('2. Email');
+    console.log('3. Senha');
+    console.log('0. Cancelar');
+    const opcao = readline.questionInt('Escolha uma opção: ');
 
-	if (validarEntradaDeDados(novosDados, ['nome', 'email', 'senha'])) {
-			try {
-					Object.assign(admParaAtualizar, novosDados);
-					console.log('ADM atualizado com sucesso:', admParaAtualizar);
-			} catch (error) {
-					console.error('Erro ao atualizar ADM:', error);
-			}
-	}
+    switch (opcao) {
+      case 1:
+        const novoNome = readline.question('Digite o novo nome (ou pressione Enter para manter o mesmo): ');
+        if (novoNome) {
+          admParaAtualizar.nome = novoNome;
+          console.log('Nome atualizado com sucesso.');
+        } else {
+          console.log('Nome não atualizado.');
+        }
+        break;
+      case 2:
+        const novoEmail = readline.question('Digite o novo email (ou pressione Enter para manter o mesmo): ');
+        if (novoEmail) {
+          admParaAtualizar.email = novoEmail;
+          console.log('Email atualizado com sucesso.');
+        } else {
+          console.log('Email não atualizado.');
+        }
+        break;
+      case 3:
+        const novaSenha = readline.question('Digite a nova senha (ou pressione Enter para manter a mesma): ');
+        if (novaSenha) {
+          admParaAtualizar.senha = novaSenha;
+          console.log('Senha atualizada com sucesso.');
+        } else {
+          console.log('Senha não atualizada.');
+        }
+        break;
+      case 0:
+        console.log('Operação de atualização cancelada.');
+        break;
+      default:
+        console.log('Opção inválida.');
+        break;
+    }
+  } catch (error) {
+    console.error('Erro ao atualizar ADM:', error);
+  }
 }
 
 // Função para deletar ADM
 function deletarAdm() {
-	console.log('\n** Deletar ADM **');
-	const id = readline.question('Digite o ID do ADM a ser deletado: ');
+  console.log('\n** Deletar ADM **');
+  const nomeOuEmail = readline.question('Digite o nome ou email do ADM que deseja deletar: ');
 
-	if (!Adm[id]) {
-			console.log('ADM não encontrado para exclusão.');
-			return;
-	}
+  try {
+    // Procurar o ADM com base no nome ou email fornecido
+    const admParaDeletar = Adm.find((adm) => adm.nome === nomeOuEmail || adm.email === nomeOuEmail);
 
-	try {
-			Adm.splice(id, 1);
-			console.log('ADM removido com sucesso!');
-	} catch (error) {
-			console.error('Erro ao deletar ADM:', error);
-	}
+    if (!admParaDeletar) {
+      console.log('ADM não encontrado para exclusão.');
+      return;
+    }
+
+    // Mostrar os dados do ADM antes da exclusão
+    console.log('Dados do ADM a ser deletado:');
+    console.log(`Nome: ${admParaDeletar.nome}`);
+    console.log(`Email: ${admParaDeletar.email}`);
+    console.log(`Senha: ${admParaDeletar.senha}`);
+
+    console.log('\nDeseja realmente excluir este ADM?');
+    console.log('1. Sim');
+    console.log('2. Não');
+    const confirmaDelecao = readline.questionInt('Escolha uma opção: ');
+
+    if (confirmaDelecao === 1) {
+      const index = Adm.indexOf(admParaDeletar);
+      if (index !== -1) {
+        Adm.splice(index, 1);
+        console.log('ADM removido com sucesso!');
+      } else {
+        console.log('Erro ao remover ADM.');
+      }
+    } else if (confirmaDelecao === 2) {
+      console.log('Operação de exclusão cancelada.');
+    } else {
+      console.log('Opção inválida. Nenhum dado foi removido.');
+    }
+  } catch (error) {
+    console.error('Erro ao deletar ADM:', error);
+  }
 }
-
-
-
-// CRUD ALUNO
-
-// CRUD EMPRESTIMO
-
-// CRUD INSTRUMENTO
-
-// CRUD MANUTENÇÃO
-
 
 // Função para o menu do Administrador
 function menuAdministrador() {
-	while (true) {
-			console.log('\n********** MENU ADMINISTRADOR **********');
-			console.log('1. Criar ADM');
-			console.log('2. Listar ADMs');
-			console.log('3. Buscar ADM por Nome');
-			console.log('4. Atualizar ADM');
-			console.log('5. Deletar ADM');
-			console.log('0. Voltar');
-			console.log('*******************************************');
-			const opcaoAdm = readline.questionInt('Escolha uma opção: ');
+  while (true) {
+    console.log('\n********** MENU ADMINISTRADOR **********');
+    console.log('1. Criar ADM');
+    console.log('2. Listar ADMs');
+    console.log('3. Buscar ADM por Nome');
+    console.log('4. Atualizar ADM');
+    console.log('5. Deletar ADM');
+    console.log('0. Voltar');
+    console.log('*******************************************');
+    const opcaoAdm = readline.questionInt('Escolha uma opção: ');
 
-			switch (opcaoAdm) {
-					case 1:
-							criarAdm();
-							break;
-					case 2:
-							listarAdms();
-							break;
-					case 3:
-							buscarAdmPorNome();
-							break;
-					case 4:
-							atualizarAdm();
-							break;
-					case 5:
-							deletarAdm();
-							break;
-					case 0:
-							console.log('Voltando ao menu principal.');
-							return; // Retorna ao menu principal
-					default:
-							console.log('Opção inválida.');
-							break;
-			}
-	}
+    switch (opcaoAdm) {
+      case 1:
+        criarAdm();
+        break;
+      case 2:
+        listarAdms();
+        break;
+      case 3:
+        buscarAdmPorNome();
+        break;
+      case 4:
+        atualizarAdm();
+        break;
+      case 5:
+        deletarAdm();
+        break;
+      case 0:
+        console.log('Voltando ao menu principal.');
+        return; // Retorna ao menu principal
+      default:
+        console.log('Opção inválida.');
+        break;
+    }
+  }
 }
 
 // Função para o menu principal
 function menuPrincipal() {
-	while (true) {
-			console.log('\n********** YALU **********');
-			console.log('\nOPÇÃO:');
-			console.log('1. Administrador');
-			console.log('2. Alunos');
-			console.log('3. Instrumentos');
-			console.log('4. Empréstimos');
-			console.log('5. Manutenção');
-			console.log('0. Sair');
-			console.log('*******************************************');
-			const opcaoPrincipal = readline.questionInt('Escolha uma opção: ');
+  while (true) {
+    console.log('\n********** YALU **********');
+    console.log('\nOPÇÃO:');
+    console.log('1. Administrador');
+    console.log('2. Alunos');
+    console.log('3. Instrumentos');
+    console.log('4. Empréstimos');
+    console.log('5. Manutenção');
+    console.log('0. Sair');
+    console.log('*******************************************');
+    const opcaoPrincipal = readline.questionInt('Escolha uma opção: ');
 
-			switch (opcaoPrincipal) {
-					case 1:
-							menuAdministrador(); // Chama o menu do Administrador
-							break;
-					case 2:
-							// Implementar as opções para Alunos
-							break;
-					case 3:
-							// Implementar as opções para Instrumentos
-							break;
-					case 4:
-							// Implementar as opções para Empréstimos
-							break;
-					case 5:
-							// Implementar as opções para Manutenção
-							break;
-					case 0:
-							console.log('\nEncerrando o programa.');
-							process.exit(0); // Encerra o programa
-							break;
-					default:
-							console.log('\nOpção inválida. Por favor, escolha uma opção válida.');
-							break;
-			}
-	}
+    switch (opcaoPrincipal) {
+      case 1:
+        menuAdministrador(); // Chama o menu do Administrador
+        break;
+      case 2:
+        // Implementar as opções para Alunos
+        break;
+      case 3:
+        // Implementar as opções para Instrumentos
+        break;
+      case 4:
+        // Implementar as opções para Empréstimos
+        break;
+      case 5:
+        // Implementar as opções para Manutenção
+        break;
+      case 0:
+        console.log('\nEncerrando o programa.');
+        process.exit(0); // Encerra o programa
+        break;
+      default:
+        console.log('\nOpção inválida. Por favor, escolha uma opção válida.');
+        break;
+    }
+  }
 }
 
-//  programa chamando o menu principal
+// Programa chamando o menu principal
 menuPrincipal();
