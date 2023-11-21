9c02da0780707b6c4f99acb13863637ec3228677
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2023-11-21 19:16:44 -0300

    ok

--- API-HTTP/meuServidor/app-3.ts
+++ API-HTTP/meuServidor/app-3.ts
@@ -12,16 +12,75 @@ const livros = [
 ];
 
 
-// Buscar todos os livros
+// Buscar todos os livros numa rota
 app.get('/livros', (req: Request, res: Response) => {
     res.send(livros)
 });
 
 
 
+// Buscar por Id numa rota
+app.get("/livros/:id", (req: Request, res: Response) => {
+    const livro = livros.find((book) => book.id === parseInt(req.params.id));
+
+    if(!livro) {
+        return res.status(404).json({mensagem: "Livro não encontrado!"});
+    }
+
+    res.send(livro);
+});
 
 
+// Adicionar livro numa rota
+app.post("/livros", (req: Request, res: Response) => {
+    const novoLivro = {
+        id: livros.length +1,
+        titulo: req.body.titulo,
+        autor: req.body.autor
+    };
+
+    livros.push(novoLivro);
+
+    res.status(201).json(novoLivro);
+
+});
+
+
+//atualizar livro numa rota
+app.put("/livros/:id", (req: Request, res: Response) => {
+    const livroIndex = livros.findIndex((book) => book.id === parseInt(req.params.id));
+
+    if(livroIndex === -1) {
+        return res.status(404).json({mensagem: "Livro não encontrado!"});
+    }
+
+    livros[livroIndex] = {
+        id: livros[livroIndex].id,
+        titulo: req.body.titulo,
+        autor: req.body.autor
+    }
+
+    res.json(livros[livroIndex]);
+});
+
+
+//deletar livro por numa rota
+app.delete("/livros/:id", (req: Request, res: Response) => {
+    const deletarLivroIndex = livros.findIndex((book) => book.id === parseInt(req.params.id));
+
+    if(deletarLivroIndex === -1) {
+        return res.status(404).json({mensagem: "Livro não encontrado!"});
+    }
+
+    livros.splice(deletarLivroIndex, 1);
+
+    res.json({mensagem: "Livro removido com sucesso!"})
+})
+
+
+
+// Ligar o servidor
 app.listen(porta, () => {
-    console.log(`Servidor typescrip rodando em http://localhost:${porta}`);
+    console.log(`Servidor typescrip rodando em http://localhost:${porta}/livros`);
 })
 
