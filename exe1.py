16a176d07f79bd6a25cbc973199e5c1091b10e87
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-06-08 17:22:49 -0300

    ok

--- exe1.py
+++ exe1.py
@@ -1,5 +1,13 @@
-num1 = float(input("Digite o primeiro número"))
-num2 = float(input("Digite o segundo número"))
-soma = num1 + num2
+def soma():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
 
-print(f'A soma é: {soma}')
+    soma = num1 + num2
+
+    return soma
+
+
+    
+# chamar função
+resultado = soma ()
+print(f'A soma é: {resultado}')
--- exe2.py
+++ exe2.py
@@ -1,8 +1,13 @@
-nomeEstudante = str(input("Digite seu nome:"))
-nota1 = float(input("Digite sua primeira nota:"))
-nota2 = float(input("Digite sua segunda nota:"))
+def mediaNota():
+    nomeAluno = input("Digite o nome do(a) aluno(a): ")
+    nota1 = float(input("Digite a primeira nota do aluno(a): "))
+    nota2 = float(input("Digite a segunda nota do aluno(a): "))
 
-media = (nota1+nota2)/2
+    media = (nota1 + nota2) / 2
+
+    return nomeAluno, nota1, nota2, media
 
 
-print(f'O estudante {nomeEstudante} com Nota 1: {nota1} e Nota 2: {nota2}, obteve a seguinte média: {media}')
+#chamar a função e printar na tela
+aluno, N1, N2, mediaFinal = mediaNota()
+print(f'O(a) aluno(a) {aluno} obteve as seguintes notas e média: \nNota 1: {N1}\nNota2: {N2}\nMédia: {mediaFinal}')
