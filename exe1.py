7a50a4755984cca4c40e75d8ca56fe9fe930b5bf
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-06-08 16:40:24 -0300

    ok

---
+++ exe1.py
@@ -0,0 +1,5 @@
+num1 = float(input("Digite o primeiro número"))
+num2 = float(input("Digite o segundo número"))
+soma = num1 + num2
+
+print(f'A soma é: {soma}')
---
+++ exe2.py
@@ -0,0 +1,8 @@
+nomeEstudante = str(input("Digite seu nome:"))
+nota1 = float(input("Digite sua primeira nota:"))
+nota2 = float(input("Digite sua segunda nota:"))
+
+media = (nota1+nota2)/2
+
+
+print(f'O estudante {nomeEstudante} com Nota 1: {nota1} e Nota 2: {nota2}, obteve a seguinte média: {media}')
