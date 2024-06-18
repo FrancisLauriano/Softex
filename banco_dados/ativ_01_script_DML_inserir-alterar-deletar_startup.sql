5946995a948249cff658c9ff0bfcb0ac43ed026c
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-06-17 22:39:37 -0300

    ok

--- banco_dados/ativ_01_script_DML_inserir-alterar-deletar_startup.sql
+++ banco_dados/ativ_01_script_DML_inserir-alterar-deletar_startup.sql
@@ -1,21 +1,13 @@
 #seleciona banco de dados
 use startup;
 
-#excluir tabela
-drop table tarefas;
-drop table membros;
-
-#desativar verificação da chave estrangeira
-SET FOREIGN_KEY_CHECKS=0;
-
-#ativar verificação da chave estrangeira
-SET FOREIGN_KEY_CHECKS=1;
-
 #inserir dados na tabela membros
 insert into membros(id_membro, nome, cargo) values (null, "Paulo", "Programador 1");
+insert into membros(id_membro, nome, cargo, genero) value(null, "Maria", "Programador 2", "F");
 
 #inserir dados na tabela tarefas
 insert into tarefas(id_tarefa, descricao, id_membro_fk) values (null, "Criar a classe produto", 1);
+insert into tarefas(id_tarefa, descricao, data_inicio, data_finalizacao, id_membro_fk) values (null, "Gerenciar banco de dados", '2023-03-01', '2024-06-16', 2);
 
 #recuperar dados da tabela membros
 select * from membros;
@@ -23,12 +15,36 @@ select nome from membros;
 
 #recuperar dados da tabela tarefas
 select * from tarefas;
+select * from membros where genero="F";
+select nome from membros where genero="F";
 
-#recuparar dados ligado as duas tabelas
+#recuparar dados ligado as tabelas membros e tarefas
 select membros.nome, tarefas.descricao
 from membros
 inner join tarefas
-ON membros.id_membro = tarefas.id_membro_FK;
+ON membros.id_membro = tarefas.id_membro_fk;
+
+
+#alterar tabela membros
+alter table membros add column genero char(1);
+
+#alterar tabela tarefas
+alter table tarefas add column data_inicio date;
+alter table tarefas add column data_finalizacao date;
+
+#atualizar dados da tabela membros
+update membros 
+set genero="M"
+where id_membro=1;
+
+#atualizar dados da tabela tarefas
+update tarefas
+set data_inicio='2024-06-01'
+where id_membro_fk=1;
+
+update tarefas 
+set data_finalizacao='2024-06-17'
+where id_membro_fk=1;
 
 #excluir dados da tabela membro
 delete from membros;
@@ -40,19 +56,3 @@ delete from membros where nome like 'silva%';
 #excluir dados da tabela tarefas
 delete from tarefas;
 delete from tarefas where id_tarefa=1;
-
-#atualizar dados da tabela membros
-update membros 
-set genero="M"
-where id_membro=1;
-
-update tarefas
-set data_inicio='2024-06-01'
-where id_membro_FK=1;
-
-
-#atualizar dados da tabela tarefas
-
-#alterar tabela
-alter table membros add column genero char(1);
-alter table tarefas add column data_inicio date;
--- banco_dados/ativ_01_script_modelo_fisico_startup.sql
+++ banco_dados/ativ_01_script_modelo_fisico_startup.sql
@@ -1,9 +1,13 @@
 #criar database (banco de dados)
 create database startup;
 
+#excluir banco de dados
+drop database startup;
+
 #selecionar (usar) database
 use startup;
 
+#criar tabela membros 
 create table membros(
 id_membro int auto_increment not null,
 nome varchar(45) not null,
@@ -11,19 +15,34 @@ cargo varchar(45) not null,
 primary key(id_membro)
 );
 
+#criar tabela tarefas com chave estrangeira
 create table tarefas(
 id_tarefa int auto_increment not null,
 descricao varchar(100) not null,
 primary key(id_tarefa),
 id_membro_fk int not null,
-foreign key(id_membro_FK) references membros(id_membro)
+foreign key(id_membro_fk) references membros(id_membro)
 );
 
+
+#criar tabela tarefas sem chave estrangeira e adicionar chave estrageira posteriormente
 create table tarefas(
 id_tarefa int auto_increment not null,
 descricao varchar(45) not null,
 primary key(id_tarefa)
 );
+
+#adicionar chave estrangeira a tabela tarefas criada anteriormente
 alter table tarefas add column id_membro_fk int not null;
 alter table tarefas add constraint id_membro_fk foreign key(id_membro_fk) references membros(id_membro);
 
+
+#desativar verificação da chave estrangeira
+SET FOREIGN_KEY_CHECKS=0;
+
+#ativar verificação da chave estrangeira
+SET FOREIGN_KEY_CHECKS=1;
+
+#excluir tabela
+drop table tarefas;
+drop table membros;
