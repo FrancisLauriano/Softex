0e77095b30f1824d2caaab943d9d5101151d8f1d
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-06-20 18:32:39 -0300

    ok

---
+++ logica/exe022.py
@@ -0,0 +1,9 @@
+# DESAFIO: Ana precisa de um sistema para coordenar os membros(nome)
+# da equipe e as tarefas (descricao) atribuídas a cada um deles.
+# Neste sistema, cada membro da equipe pode ter várias tarefas
+# atribuídas, mas uma tarefa específica está ligada apenas a um
+# membro da equipe.
+# Sugestão utilizar array e função.
+
+membros = ['Ana', 'Carlos', 'João']
+tarefas = ['Programador 1', 'Gerente de Banco de Dados', 'Gerente de Projeto']
---
+++ outros/exe001.py
@@ -0,0 +1,10 @@
+numero1 = 1
+float1 = 1.1
+bolean1 = True
+string1 = 'Francis'
+
+print('Número: ', type(numero1))
+print('Float: ', type(float1))
+print('Boleano: ', type(bolean1))
+print('String: ', type(string1))
+
