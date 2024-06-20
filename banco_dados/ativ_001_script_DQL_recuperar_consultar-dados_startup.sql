facfdb898fff9b8ced57fbc0311eae5d2fe164b3
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-06-19 21:58:40 -0300

    ok

--- banco_dados/ativ_001_script_DQL_recuperar_consultar-dados_startup.sql
+++ banco_dados/ativ_001_script_DQL_recuperar_consultar-dados_startup.sql
@@ -1,6 +1,7 @@
 #DQL - principais comando: SELECT (comandos focados em consultas de dados)
-# usar SELECT com WHERE / ORDER BY / INNER JOIN / GROUP BY e função de agregação - COUNT (contar), SUM (somar), AVG (media), MIN (minimo), e MAX (maximo)
 #recuperar dados e fazer a leitura ou manipulação deles
+#usar SELECT com WHERE (especifica os critérios da consulta) / ORDER BY (ordenar a consulta) / INNER JOIN (junção de duas ou mais tabelas) / HAVING (filtrar grupos) / GROUP BY (agrupar dados)
+#GROUP BY é comumente utilizado em conjunto com funções de agregação - COUNT (contar), SUM (somar), AVG (media), MIN (minimo), e MAX (maximo)
 
 #seleciona banco de dados - USE
 use startup;
@@ -14,8 +15,8 @@ select * from tarefas;
 select * from membros where genero="F";
 select nome from membros where genero="F";
 
-#recuperar dados da tabela ordenado de forma ascendente ou decrescente  
-# SELECT FROM / ORDER BY (por padrão, ordena de forma crescente - ASC. Para ordernar de forma descrecente usar DESC)
+#recuperar dados da tabela ordenado de forma ascendente ou decrescente - SELECT FROM / ORDER BY 
+#por padrão, ordena de forma crescente - ASC. Para ordernar de forma descrecente usar DESC
 select * from tarefas 
 order by descricao;
 
@@ -28,8 +29,21 @@ from membros
 inner join tarefas
 ON membros.id_membro = tarefas.id_membro_fk;
 
-#exibir o nome, cargo, descrição da tarefa, data inicial e data final - SELECT FROM / INNER JOIN
+#exibir o nome, cargo, descrição da tarefa, data inicial e data final - SELECT FROM / INNER JOIN (junção de duas ou mais tabelas)
 select membros.nome, membros.cargo, tarefas.descricao, tarefas.data_inicio, tarefas.data_finalizacao
 from membros
 inner join tarefas
 ON membros.id_membro = tarefas.id_membro_fk;
+
+#exibir quais os cargos existe na tabela membros - SELECT FROM /  GROUP BY
+select cargo
+from membros
+group by cargo;
+
+#exibir quantos membros do genero feminino tem na tabela membros - SELECT FROM / WHERE / COUNT
+select count(*) AS QTD_mulheres
+from membros
+where genero = 'F';
+
+
+
