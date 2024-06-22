219ec0dcb836e1a0369e7fe4ce12ed747295f300
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-06-21 23:11:23 -0300

    ok

--- outros/exe008.py
+++ outros/exe008.py
@@ -4,31 +4,19 @@
 tabuada=int(input('Informe um número: '))
 
 for i in range(11):
-    print('{} X {} = {}'.format(tabuada, +i, tabuada*(+i)))
+    print('{} X {:2} = {}'.format(tabuada, +i, tabuada*(+i)))
 
 
 # OU
 # tabuada=int(input('Informe um número: '))
 # print('TABUADA DE {}'.format(tabuada))
-# n1=tabuada*1
-# n2=tabuada*2
-# n3=tabuada*3
-# n4=tabuada*4
-# n5=tabuada*5
-# n6=tabuada*6
-# n7=tabuada*7
-# n8=tabuada*8
-# n9=tabuada*9
-# n10=tabuada*10
-# print('{}x1={}'.format(tabuada, n1))
-# print('{}x2={}'.format(tabuada, n2))
-# print('{}x3={}'.format(tabuada, n3))
-# print('{}x4={}'.format(tabuada, n4))
-# print('{}x5={}'.format(tabuada, n5))
-# print('{}x6={}'.format(tabuada, n6))
-# print('{}x7={}'.format(tabuada, n7))
-# print('{}x8={}'.format(tabuada, n8))
-# print('{}x9={}'.format(tabuada, n9))
-# print('{}x10={}'.format(tabuada, n10))
-
-
+# print('{} x {:2} = {}'.format(tabuada, 1, tabuada*1))
+# print('{} x {:2} = {}'.format(tabuada, 2, tabuada*2))    
+# print('{} x {:2} = {}'.format(tabuada, 3, tabuada*3))
+# print('{} x {:2} = {}'.format(tabuada, 4, tabuada*4))
+# print('{} x {:2} = {}'.format(tabuada, 5, tabuada*5))
+# print('{} x {:2} = {}'.format(tabuada, 6, tabuada*6))    
+# print('{} x {:2} = {}'.format(tabuada, 7, tabuada*7))
+# print('{} x {:2} = {}'.format(tabuada, 8, tabuada*8))
+# print('{} x {:2} = {}'.format(tabuada, 9, tabuada*9))
+# print('{} x {:2} = {}'.format(tabuada, 10, tabuada*10))
